f4f498be6ce5d892ac4d789a3b44b3a82e604df1
s@lamandra <ussashok09@gmail.com>
2020-06-15 18:00:17 -0700

    add try spec

--- src/test/scala/TrySpec.scala
+++ src/test/scala/TrySpec.scala
@@ -3,103 +3,141 @@
 import org.scalatest._
 
 import scala.util.Try
+import scala.util.Success
+import scala.util.Failure
 
 object TrySpec {
 
 
-  case class Product(property: String) {
-    def eat: String = {
-      property match {
-        case "edible" => property
-        case "inedible" => throw new Exception(s"Product is inedible")
-        case _ => throw new Exception(s"Product is undefined with property = $property")
+  case class Product(name: String) {
+    def eat: Product = {
+      name.toLowerCase match {
+        case "orange" => this
+        case "stone" => throw new Exception(s"Product is inedible")
+        case _ => throw new Exception(s"Product with undefined name = $name")
       }
     }
   }
 
   object Product {
-    val edibleProduct: Product = Product("edible")
-    val inedibleProduct: Product = Product("inedible")
-    val undefinedProduct: Product = Product("abracadabra")
+    val orange: Product = Product("orange") // orange
+    val stone: Product = Product("stone") // stone
+    val productNoName: Product = Product("abracadabra") // (?)
   }
 
 }
 
 class TrySpec extends FlatSpec with Matchers {
 
+  import TrySpec.Product
+  import TrySpec.Product._
 
   it should "Try create" in {
-    // Try is Box  => Red Green or Non Defined
+    //     Try is Box  => Try (Yellow), Success - Green (with result), Failure - red (with exception)
 
-    //    val list0Int: List[Int] = Nil
-    //    list0Int shouldBe List()
-    //
-    //    // add new matrioshka
-    //    val list1Int: List[Int] = 30 :: Nil
-    //    list1Int shouldBe List(30)
-    //
-    //    val list2Int: List[Int] = 20 :: 30 :: Nil
-    //    list2Int shouldBe List(20, 30)
-    //
-    //    val list3Int: List[Int] = 10 :: list2Int
-    //    list3Int shouldBe List(10, 20, 30)
-
+    Try(orange.eat) shouldBe Success(orange)
+    Try(stone.eat).isFailure shouldBe true
+    println(Try(stone.eat))
+    Try(productNoName.eat).isFailure shouldBe true
+    println(Try(productNoName.eat))
+    Try(productNoName.eat) != Try(stone.eat) shouldBe true
   }
 
   it should "Try for map" in {
 
-    //    val list3Int: List[Int] = List(10, 20, 30)
-    //    list3Int.map(_ + 2) shouldBe List(12, 22, 32)
-    //
-    //    val emptyList: List[Int] = List.empty
-    //    emptyList.map(_ + 2) shouldBe List()
+    Try(orange.eat).map(_.name + " can eat") shouldBe Success(orange.name + " can eat")
+    Try(stone.eat).map(_.name + " can eat").isFailure shouldBe true
   }
 
 
   it should "Try flatten" in {
-    //    val listNested3Int: List[List[Int]] = List(List(10), List(20, 30))
-    //    listNested3Int.flatten shouldBe List(10, 20, 30)
-    //
-    //    val listNested3IntOther: List[List[Int]] = List(List(10), List(20), List(30))
-    //    listNested3IntOther.flatten shouldBe List(10, 20, 30)
-    //
-    //    val listNested2Int: List[List[Int]] = List(List(), List(20, 30), List())
-    //    listNested2Int.flatten shouldBe List(20, 30)
-    //
-    //    val listNested0Int: List[List[Int]] = List(List())
-    //    listNested0Int.flatten shouldBe List()
+    val successSuccessOrange: Try[Try[Product]] = Try(Try(orange.eat))
+    successSuccessOrange.flatten shouldBe Success(orange.eat)
+
+    val failureStone = Try(stone.eat)
+    val successFailedStone: Try[Try[Product]] = Try(failureStone)
+    successFailedStone.flatten shouldBe failureStone
+
+    val failure = Failure(new Exception("e"))
+    val failureExceptions: Try[Try[Product]] = failure
+    failureExceptions.flatten shouldBe failure
 
     // flatMap = flatten + map
   }
 
-
   it should "Try filter" in {
-    //    val list3Int: List[Int] = List(10, 20, 30)
-    //    list3Int.filter(_ < 10) shouldBe List()
-    //    list3Int.filter(_ > 10) shouldBe List(20, 30)
-    //
-    //    val emptyList: List[Int] = List.empty
-    //    emptyList.filter(_ < 10) shouldBe List()
-    //    emptyList.filter(_ > 10) shouldBe List()
+    Try(orange.eat).filter(_.name.nonEmpty) shouldBe Success(orange)
+    Try(orange.eat).filter(_.name.isEmpty).isFailure shouldBe true
+
+    Try(stone.eat).filter(_.name.nonEmpty).isFailure shouldBe true
+    Try(stone.eat).filter(_.name.isEmpty).isFailure shouldBe true
+
   }
 
+  it should "Try recover" in {
+    Try(orange.eat).recover { case _: Exception => productNoName } shouldBe Success(orange.eat)
+    Try(stone.eat).recover { case _: Exception => productNoName } shouldBe Success(productNoName)
 
-  //todo add getOrElse // add example bad style
-  //todo  bad style  example if(try.isSuccess) try.get else ??? good stile ....
-  //todo add orElse
-  //todo add recover
-  //todo add recoverWith
-  //todo add toOption
-  //todo example with validation List[Product] => List[Exceptions]
+  }
 
+  it should "Try recoverWith" in {
+    Try(orange.eat).recoverWith { case _: Exception => Success(productNoName) } shouldBe Success(orange.eat)
+    Try(stone.eat).recoverWith { case _: Exception => Success(productNoName) } shouldBe Success(productNoName)
+
+  }
+
+  it should "Try getOrElse" in {
+    Try(orange.eat).getOrElse(productNoName) shouldBe orange.eat
+    Try(stone.eat).getOrElse(productNoName) shouldBe productNoName
+
+
+    // get not using!!!
+    Try(Try(stone.eat).get).isFailure shouldBe true
+  }
+
+  it should "Try !!! bad style !!! " in {
+
+    val successOrange = Try(orange.eat)
+
+    //bad style
+    val myOrange = if (successOrange.isSuccess) successOrange.get else ???
+    myOrange shouldBe orange.eat
+
+
+    val failureStone = Try(stone.eat)
+
+    //bad style
+    val tryStone = Try(if (failureStone.isFailure) failureStone.get else ???)
+    tryStone.isFailure
+  }
+
+  it should "Try orElse" in {
+    Try(orange.eat).orElse(Success(productNoName)) shouldBe Success(orange.eat)
+    Try(stone.eat).orElse(Success(productNoName)) shouldBe Success(productNoName)
+  }
+
+  it should "Try toOption" in {
+    Try(orange.eat).toOption shouldBe Some(orange.eat)
+    Try(stone.eat).toOption shouldBe None
+  }
 
   it should "Try flatten 3x" in {
-    //    val listDeepNested4Int: List[List[List[Int]]] = List(List(List(1, 1), List(2)), Nil, List(List(3), List(4)), Nil)
-    //
-    //    listDeepNested4Int.flatten shouldBe List(List(1, 1), List(2), List(3), List(4))
-    //
-    //    val listDeepNested0Int: List[List[List[Int]]] = List(List(List()))
-    //    listDeepNested0Int.flatten shouldBe List(List())
+    val sssProduct: Try[Try[Try[Product]]] = Try(Try(Try(orange.eat)))
+    sssProduct.flatten shouldBe Success(Success(orange.eat))
+
+    val failure = Failure(new Exception("e"))
+
+    val ssfProduct: Try[Try[Try[Product]]] = Success(Success(failure))
+
+    ssfProduct.flatten shouldBe Success(failure)
+
+    val sfProduct: Try[Try[Try[Product]]] = Success(failure)
+
+    sfProduct.flatten shouldBe failure
+
+    val fProduct: Try[Try[Try[Product]]] = failure
+
+    fProduct.flatten shouldBe failure
 
   }
 
